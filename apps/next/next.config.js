61a3d66b5483c5eb5d34155a54750b88a6c7ffd6
Abdulraoof <ali.aldabil@gmail.com>
2023-08-27 18:23:36 +0300

    cleanup

--- apps/next/next.config.js
+++ apps/next/next.config.js
@@ -1,12 +1,6 @@
-const nextBuildId = require('next-build-id');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  generateBuildId: () => nextBuildId({ dir: __dirname }),
   swcMinify: true,
-  experimental: {
-    appDir: true,
-  },
   transpilePackages: ['utils'],
 };
 

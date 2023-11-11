6b03ba6a84d2237d5f9e71533a895a24215b03f7
Abdulraoof <ali.aldabil@gmail.com>
2023-11-11 03:26:32 +0300

    GTM

--- apps/next/src/app/[lang]/layout.tsx
+++ apps/next/src/app/[lang]/layout.tsx
@@ -17,6 +17,7 @@ export const generateStaticParams = () => {
 };
 
 const LangRootLayout: NextLayout<{ auth: React.ReactNode }> = ({ children, params: { lang } }) => {
+  const GTM_ID = process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER;
   const cookiesList = cookies();
 
   return (
@@ -27,9 +28,31 @@ const LangRootLayout: NextLayout<{ auth: React.ReactNode }> = ({ children, param
           dangerouslySetInnerHTML={{ __html: JSON.stringify(globalJsonLd) }}
           type='application/ld+json'
         />
+
+        {/* GTM */}
+        {GTM_ID ? (
+          <script>
+            {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${GTM_ID}');
+          `}
+          </script>
+        ) : null}
       </head>
+
       <body className={`${cairoFont.variable} bg-body font-cairo text-text`}>
         <Layout lang={lang}>{children}</Layout>
+
+        {GTM_ID ? (
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_ID}" height="0" width="0" style="display: none; visibility: hidden;"></iframe>`,
+            }}
+          />
+        ) : null}
       </body>
     </html>
   );

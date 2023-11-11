004392b4c5304e3c865a7e7b8776df955462ac7d
Abdulraoof <ali.aldabil@gmail.com>
2023-11-11 05:15:22 +0300

    breadcrumb padding

--- apps/next/src/components/common/breadcrumb.tsx
+++ apps/next/src/components/common/breadcrumb.tsx
@@ -26,7 +26,7 @@ export const Breadcrumb = ({ lang, items }: Props) => {
             key={item.link}
             lang={lang}
           >
-            {i > 0 && <ChevronIcon className='h-4 w-4 rtl:rotate-180' />}
+            {i > 0 && <ChevronIcon className='h-4 w-4 shrink-0 rtl:rotate-180' />}
             {/* last item */}
             {i === items.length - 1 ? (
               <span aria-current='page' className='px-1 py-2'>
@@ -34,7 +34,7 @@ export const Breadcrumb = ({ lang, items }: Props) => {
               </span>
             ) : (
               <Link
-                className='hover:text-primary-light line-clamp-1 px-1 py-2'
+                className='hover:text-primary-light line-clamp-1 block w-full p-4 px-1 ltr:pr-3 rtl:pl-3'
                 href={`/${lang}/${item.link}`}
               >
                 {item.title}

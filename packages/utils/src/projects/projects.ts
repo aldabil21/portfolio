7d78ed6e780332a2e912c052f5a1bd17af277f0d
Abdulraoof <ali.aldabil@gmail.com>
2023-12-01 18:59:49 +0300

    Add more projects

--- packages/utils/src/projects/projects.ts
+++ packages/utils/src/projects/projects.ts
@@ -10,6 +10,14 @@ export type Project = {
 
 export const projectsList: Project[] = [
   {
+    project: 'webook',
+    slug: 'webook',
+    description: [1, 2, 3],
+    thumbnail: '/images/webook/thumbnail.png',
+    numberOfImages: 7,
+    live: 'https://webook.com',
+  },
+  {
     project: 'riyadhseason',
     slug: 'riyadh-season-23-24',
     description: [1, 2],
@@ -34,12 +42,12 @@ export const projectsList: Project[] = [
     live: 'https://tickets.victoryarena.com',
   },
   {
-    project: 'afcon',
-    slug: 'africa-cup-of-nations-2023',
-    description: [1, 2, 3],
-    thumbnail: '/images/afcon/thumbnail.png',
-    numberOfImages: 5,
-    live: 'https://frmftickets.ma',
+    project: 'react-scheduler',
+    slug: 'react-scheduler',
+    description: [1],
+    thumbnail: '/images/react-scheduler/thumbnail.png',
+    numberOfImages: 1,
+    live: 'https://github.com/aldabil21/react-scheduler',
   },
   {
     project: 'misk',
@@ -48,6 +56,14 @@ export const projectsList: Project[] = [
     thumbnail: '/images/misk/thumbnail.png',
     numberOfImages: 12,
     live: 'https://eventhub.misk.org.sa',
+  },
+  {
+    project: 'afcon',
+    slug: 'africa-cup-of-nations-2023',
+    description: [1, 2, 3],
+    thumbnail: '/images/afcon/thumbnail.png',
+    numberOfImages: 5,
+    live: 'https://frmftickets.ma',
   },
   {
     project: 'sagp',
@@ -88,5 +104,21 @@ export const projectsList: Project[] = [
     thumbnail: '/images/neom/thumbnail.png',
     numberOfImages: 6,
     live: 'https://theline.halayalla.com',
+  },
+  {
+    project: 'easy-webpush',
+    slug: 'easy-webpush',
+    description: [1],
+    thumbnail: '/images/easy-webpush/thumbnail.png',
+    numberOfImages: 4,
+    live: 'https://github.com/aldabil21/opencart-easywebpush',
+  },
+  {
+    project: 'european-honey',
+    slug: 'european-honey',
+    description: [1],
+    thumbnail: '/images/european-honey/thumbnail.png',
+    numberOfImages: 4,
+    live: 'https://europeanhoney.com',
   },
 ];
